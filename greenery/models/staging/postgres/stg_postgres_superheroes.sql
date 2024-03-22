3f3b99f8670902c4e2ebab8e8642a48666d20964
Derek Byrne <derek.byrne@globalp.com>
2024-03-22 17:04:38 +0000

    Assignment#1submission

---
+++ greenery/models/staging/postgres/stg_postgres_superheroes.sql
@@ -0,0 +1,3 @@
+{{ config(materialized='table') }}
+select ID, NAME, GENDER, EYE_COLOR, RACE, HAIR_COLOR, HEIGHT, PUBLISHER, SKIN_COLOR, ALIGNMENT, WEIGHT, CREATED_AT, UPDATED_AT 
+from {{source('postgres','superheroes')}}
